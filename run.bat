e7a28e642ad9119650a836988a9b7b5f51d698c7
王小茶 <543102602@qq.com>
2025-10-16 11:28:14 +0800

    优化了run.bat的路径获取

--- run.bat
+++ run.bat
@@ -1,12 +1,14 @@
 @echo off
 REM 设置C++程序的位置
-set CPP_PROGRAM=D:\cxx workshop\StarNet\build\Release\HGRSTAR.exe
+set "CPP_PROGRAM=%cd%\build\Release\HGRSTAR.exe"
 
 REM 设置包含JSON文件的目录
-set JSON_DIR=D:\cxx workshop\StarNet\json
+set "JSON_DIR=%cd%\json"
 
 REM 进入JSON文件所在的目录
 cd /d %JSON_DIR%
+
+echo "%CPP_PROGRAM%"
 
 REM 遍历当前目录下的所有JSON文件，并调用C++程序处理每个文件
 for %%f in (*.json) do (
