9972aef377a2a8368a4658985752ea20a65b15af
王小茶 <543102602@qq.com>
2025-10-22 02:09:33 +0800

    新增了StarNet2510152000的实验json文件（四阶段的深度为 [2, 2, 8, 3]）

---
+++ Standby/StarNet2510152000A64Embed2283Depth/rename.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "folder=%cd%"
+set "old_part=2510152000A64Embed"
+set "new_part=2510152000A64Embed2283Depth"
+set "search_str=StarNet%old_part%"
+set "replace_str=StarNet%new_part%"
+
+for %%f in ("%folder%\StarNet??????????????????_s??.json") do (
+    set "filename=%%~nxf"
+    set "prefix=!filename:~0,7!"
+    set "suffix=!filename:~18!"
+    set "new_name=!prefix!%new_part%!suffix!"
+    ren "!filename!" "!new_name!"
+
+    set "temp_file=!new_name!.tmp"
+    if exist "!temp_file!" del "!temp_file!"
+    for /f "usebackq delims=" %%I in ("!new_name!") do (
+        set "current_line=%%I"
+        set "modified_line=!current_line:%search_str%=%replace_str%!"
+        (echo !modified_line!) >> "!temp_file!"
+    )
+
+    copy /y "!temp_file!" "!new_name!" > nul
+    if exist "!temp_file!" del "!temp_file!"
+)
